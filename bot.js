1fd9c617931f9f42810fb9620fa6346af28c54d3
medo67 <45091642+medo67@users.noreply.github.com>
2019-01-28 16:46:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,15 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
-         client.on('message', message => {
-            if (message.content === 'welcome to GS :heart:️,') {
-              message.channel.send('****Welcome To Games Soceity :heart: 
+
+client.on('message', msg => {
+  if message.channel.send('****Welcome To Games Soceity :heart: 
           -     You Are in Big Society****');
-
-               
-
-            }
+    msg.reply('Pong!');
+  }
 });
+
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
