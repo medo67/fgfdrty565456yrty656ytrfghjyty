7c380f28179b6f0028453f81869cc4d32ae1846d
medo67 <45091642+medo67@users.noreply.github.com>
2019-01-28 17:15:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,14 +2,16 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(Logged in as ${client.user.tag}!);
 });
+         client.on('message', message => {
+            if (message.content === 'welcome to GS :heart:️') {
+              message.channel.send('**Welcome To Games Soceity :heart: 
+                                     -     You Are in Big Society**');
 
-client.on('welcome to GS :heart:️,', msg => {
- if (msg.content === 'welcome to GS :heart:️,') {
-  msg.reply('Welcome To Games Soceity :heart: 
-          -     You Are in Big Society');
-  }
+              message.channel.sendFile("./photoshop.PNG");
+
+
+            }
 });
-
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
