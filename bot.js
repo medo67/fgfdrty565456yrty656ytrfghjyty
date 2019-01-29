5174e9e02ea7549c0c63e246b8dbc534a0fbaba4
medo67 <45091642+medo67@users.noreply.github.com>
2019-01-29 16:25:03 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -37,6 +37,25 @@ if (message.content.startsWith(adminprefix + 'avatar')) {
     message.channel.send(`Changing The Avatar To :**${argresult}** `);
 }
 });
+client.on('ready',async () => {
+  let GUILDID = '477702574447919106'; 
+  let CHANNELID = '539219700203126834'; 
+  voiceStay(GUILDID, CHANNELID);
+  function voiceStay(guildid, channelid) {
+    if(!guildid) throw new Error('Syntax: voiceStay function requires guildid');
+    if(!channelid) throw new Error('Syntax: voiceStay function requires channelid');
 
+    let guild = client.guilds.get(guildid);
+    let channel = guild.channels.get(channelid);
+
+    if(channel.type === 'voice') {
+      channel.join().catch(e => {
+        console.log(Failed To Join :: ${e.message});
+      });
+    } else {
+      console.log(Channel Type :: ${channel.type}, It must be Voice.);
+    }
+  }
+});
 
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
